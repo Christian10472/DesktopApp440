3613a8aaced424a48bef137253d52457517dac27
Christian Perez <christian.perez.935@my.csun.edu>
2023-10-03 19:49:41 -0700

    Hello World!

--- src/main/java/com/example/desktopapp440/HelloApplication.java
+++ src/main/java/com/example/desktopapp440/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+//
     public static void main(String[] args) {
         launch();
     }
