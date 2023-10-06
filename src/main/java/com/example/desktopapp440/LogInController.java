97c4c5553b8fd31751df811b906ee5ecf32902b6
anthony <anthony.plasencia29511@gmail.com>
2023-10-05 20:25:57 -0700

    getting from laptop to pc broken code

---
+++ src/main/java/com/example/desktopapp440/LogInController.java
@@ -0,0 +1,24 @@
+package com.example.desktopapp440;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInController {
+    @FXML
+    private Button LoginButton;
+    @FXML
+    private Button SignUpButton;
+    @FXML
+    private TextField UserName_Field;
+    @FXML
+    private PasswordField Password_Field;
+
+    @FXML
+    protected void onHelloButtonClick() {
+
+
+    }
+}
