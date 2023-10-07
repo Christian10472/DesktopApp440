bc9f90c5cd773318c1dcc25a5e3b1a524ee55408
Christian Perez <christian.perez.935@my.csun.edu>
2023-10-06 22:59:55 -0700

    Added Sign Up controller

--- src/main/java/com/example/desktopapp440/SIgnUpController.java
+++ src/main/java/com/example/desktopapp440/SIgnUpController.java
@@ -1,20 +1,47 @@
 package com.example.desktopapp440;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SIgnUpController {
     private boolean allGood;
     @FXML
-    private Label FirstText, LastText, UsernameText, EmailText, PasswordText;
+    private Label FirstText, UsernameText, EmailText, PasswordText;
+    @FXML
+    private TextField FirstUser, LastUser, UsernameUser, EmailUser, PasswordUser;
+    @FXML
+    private CheckBox TermsCheck;
 
     @FXML
     protected void onSignUpClick() {
-        ifNull(FirstText);
+        allGood = true;
+        //Checking if any parameters are null
+        if((isNull(FirstUser) == true) || (isNull(LastUser) == true)){
+            FirstText.setText("*Please enter a valid name");
+        }
+        if(isNull(UsernameUser) == true){
+            UsernameText.setText("*Please enter a valid Username");
+        }
+        if(isNull(EmailUser) == true){
+            EmailText.setText("*Please enter a valid email address");
+        }
+        if(isNull(PasswordUser) == true){
+            PasswordText.setText("*Please enter a valid email password");
+        }
+
+        //checking if parameters are valid to database
+
+
     }
 
-    public void ifNull(Label x){
-
+    public boolean isNull(TextField x){
+        if (x.getText().isEmpty()){
+            allGood = false;
+            return true;
+        }
+        return false;
     }
 
 }
