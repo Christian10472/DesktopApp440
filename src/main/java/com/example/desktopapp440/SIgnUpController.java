38d33b2c60ac7b71eee765d7e08722b0edaf7a30
Christian Perez <christian.perez.935@my.csun.edu>
2023-10-07 23:30:53 -0700

    Worked out issues in SIgnUpController

--- src/main/java/com/example/desktopapp440/SIgnUpController.java
+++ src/main/java/com/example/desktopapp440/SIgnUpController.java
@@ -1,11 +1,17 @@
 package com.example.desktopapp440;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.sql.*;
+
 public class SIgnUpController {
+
+    private String password = "root";
+    private String sqlPass ;
     private boolean allGood;
     @FXML
     private Label FirstText, UsernameText, EmailText, PasswordText;
@@ -13,11 +19,34 @@ public class SIgnUpController {
     private TextField FirstUser, LastUser, UsernameUser, EmailUser, PasswordUser;
     @FXML
     private CheckBox TermsCheck;
+    @FXML
+    private Button SignUp;
 
+    @FXML
+    protected void onTermsCheckOnClick(){
+        if (TermsCheck.isSelected()){
+            SignUp.setDisable(false);
+        }else{
+            SignUp.setDisable(true);
+        }
+    }
     @FXML
     protected void onSignUpClick() {
         allGood = true;
         //Checking if any parameters are null
+        isUserPropmptEmpty();
+        //checking if parameters are valid to database
+        if(allGood == true){
+            doesUserExist();
+            if(allGood == true){
+                addUser();
+            }
+
+        }
+
+    }
+
+    public void isUserPropmptEmpty(){
         if((isNull(FirstUser) == true) || (isNull(LastUser) == true)){
             FirstText.setText("*Please enter a valid name");
         }
@@ -30,10 +59,6 @@ public class SIgnUpController {
         if(isNull(PasswordUser) == true){
             PasswordText.setText("*Please enter a valid email password");
         }
-
-        //checking if parameters are valid to database
-
-
     }
 
     public boolean isNull(TextField x){
@@ -42,6 +67,52 @@ public class SIgnUpController {
             return true;
         }
         return false;
+    }
+
+    public void doesUserExist(){
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/desktopappdb", "root", sqlPass);
+            System.out.println("rahhh");
+            String SQL = "SELECT * FROM Users WHERE Username = ?;";
+            PreparedStatement pstmt = con.prepareStatement(SQL);
+            pstmt.setString(1, UsernameUser.getText());
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                allGood = false;
+            }
+
+            SQL = "SELECT * FROM Users WHERE Email = ?;";
+            pstmt = con.prepareStatement(SQL);
+            pstmt.setString(1, EmailUser.getText());
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+               allGood = false;
+            }
+        } catch (Exception e) {
+            System.out.println("Error connecting to database");
+            System.out.println(e.getMessage());
+            allGood = false;
+        }
+    }
+
+    public void addUser(){
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/desktopappdb", "root", sqlPass);
+            String SQL = "INSERT INTO Users(Username, Password, FirstName, LastName, Email) VALUES(?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(SQL);
+            pstmt.setString(1, UsernameUser.getText());
+            pstmt.setString(2, PasswordUser.getText());
+            pstmt.setString(3, FirstUser.getText());
+            pstmt.setString(4, LastUser.getText());
+            pstmt.setString(5, EmailUser.getText());
+            pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("Error inserting into table");
+            System.out.println(e.getMessage());
+            allGood = false;
+        }
     }
 
 }
