f5ba015c60607a00ce04d208fa583cdaa0e11287
anthony <anthony.plasencia29511@gmail.com>
2023-10-07 19:06:01 -0700

    LogInController still not working

--- src/main/java/com/example/desktopapp440/LogInController.java
+++ src/main/java/com/example/desktopapp440/LogInController.java
@@ -2,23 +2,57 @@ package com.example.desktopapp440;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.sql.*;
+
 public class LogInController {
-    @FXML
-    private Button LoginButton;
-    @FXML
-    private Button SignUpButton;
-    @FXML
-    private TextField UserName_Field;
-    @FXML
-    private PasswordField Password_Field;
+    private String Username_Input;
+    private String Password_Input;
+    private String SelectStatement = "select username , Password from users where username = ? and Password = ?";
+
+    private String sqlUser = "root";
+    private String sqlPass = "BonkRipper420$";
 
     @FXML
-    protected void onHelloButtonClick() {
+    Button LoginButton;
+    @FXML
+    Button SignUpButton;
+    @FXML
+    TextField UserName_Field;
+    @FXML
+    PasswordField Password_Field;
 
 
+    @FXML
+    public void onLogInButtonClick() {
+        if(ValidateInput()==true){
+            System.out.println("success");
+        }
+    }
+
+    private boolean ValidateInput() {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/desktopappdata", sqlUser, sqlPass);
+             PreparedStatement preparedStatement = connection.prepareStatement(SelectStatement)) {
+            preparedStatement.setString(1, UserName_Field.getText());
+            preparedStatement.setString(2, Password_Field.getText());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+
+                if (UserName_Field.getText().equals(resultSet.getString("Username")) && Password_Field.getText().equals(resultSet.getString("Password"))) {
+                    UserName_Field.setText("Success data connection");
+
+                    return true;
+                }
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("error");
+        }
+
+        return false;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.desktopapp440 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.desktopapp440 to javafx.fxml;
