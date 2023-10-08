c058b9e33d5ca4c64ffe270e35c3800cb760d077
anthony <anthony.plasencia29511@gmail.com>
2023-10-07 21:47:33 -0700

    LogInController still not working

--- src/main/java/com/example/desktopapp440/LogInController.java
+++ src/main/java/com/example/desktopapp440/LogInController.java
@@ -8,12 +8,12 @@ import javafx.scene.control.TextField;
 import java.sql.*;
 
 public class LogInController {
-    private String Username_Input;
-    private String Password_Input;
-    private String SelectStatement = "select username , Password from users where username = ? and Password = ?";
 
-    private String sqlUser = "root";
-    private String sqlPass = "BonkRipper420$";
+     String SelectStatement = "select Username , Password from users where Username = ? and Password = ?";
+
+     String sqlUser = "root";
+     String sqlPass = "BonkRipper420$";
+    String url = "jdbc:mysql://localhost:3306/desktopappdb";
 
     @FXML
     Button LoginButton;
@@ -27,30 +27,34 @@ public class LogInController {
 
     @FXML
     public void onLogInButtonClick() {
-        if(ValidateInput()==true){
-            System.out.println("success");
+        if(ValidateInput()){
+            System.out.println("all good");
         }
     }
 
     private boolean ValidateInput() {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/desktopappdata", sqlUser, sqlPass);
-             PreparedStatement preparedStatement = connection.prepareStatement(SelectStatement)) {
-            preparedStatement.setString(1, UserName_Field.getText());
-            preparedStatement.setString(2, Password_Field.getText());
+        try (
+            Connection connection = DriverManager.getConnection(url, sqlUser, sqlPass)){
+            System.out.println("DBConnected");
+            PreparedStatement preparedStatement = connection.prepareStatement(SelectStatement);
 
+            String UsernameInput = UserName_Field.getText();
+            String PasswordInput = Password_Field.getText();
+
+            preparedStatement.setString(1, UsernameInput);
+            preparedStatement.setString(2, PasswordInput);
+
+
+            System.out.println("Connection");
             ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()) {
-
-                if (UserName_Field.getText().equals(resultSet.getString("Username")) && Password_Field.getText().equals(resultSet.getString("Password"))) {
-                    UserName_Field.setText("Success data connection");
-
+            if (resultSet.next()) {
                     return true;
-                }
             }
 
 
         } catch (SQLException e) {
-            System.out.println("error");
+            e.printStackTrace();
+            System.out.println("SQL Error: " + e.getMessage());
         }
 
         return false;
